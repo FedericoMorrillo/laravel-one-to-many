39a4b98a867f7ed01a799255fcd3a9a28c9bbd23
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-02-11 18:58:41 +0100

    feat: relazione one to many projects-types

---
+++ database/migrations/2024_02_11_173303_add_field_type_id_foreign_table_project.php.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Reference\Reference;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_id')->nullable();
+
+            $table->foreign('type_id')
+                ->references('id')
+                ->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->dropForeign('project_type_id_foreign');
+            $table->dropColumn('type_id');
+        });
+    }
+};
